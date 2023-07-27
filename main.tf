4ac3567e7e66baf6a0b55cc630ca788dfa51c25c
spandanakoppaku <skoppaku@gmail.com>
2023-07-27 19:47:22 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  source        = "./vendor/modules/vpc" 
-  VPC_CIDR      = var.VPC_CIDR
-
-tags = {
-    Name = "roboshop-vpc"
-  }
-
+source                 = "./vendor/modules/vpc" 
+VPC_CIDR               = var.VPC_CIDR
+ENV                    = var.ENV
+AZ                     = var.AZ
+PUBLIC_SUBNET_CIDR     = var.PUBLIC_SUBNET_CIDR
+PRIVATE_SUBNET_CIDR    = var.PRIVATE_SUBNET_CIDR
+   
 }
